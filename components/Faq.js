e82f616e4c7fde34ead8de2879685ac340d0b9a1
S Dinesh <d4dinesh52@gmail.com>
2022-12-15 13:28:51 +0530

    faq issue fixed

--- components/Faq.js
+++ components/Faq.js
@@ -5,15 +5,15 @@ const Faq = () => {
         {
             title: "How to hire Dedicated Developers from Grids & Guides?",
             desc: <div>
-                        <p>To hire developers from us, Clients have just to follow a few steps:</p>
-                        <p>a. Approach us with the requirements of the project and developers in brief.</p>
-                        <p>b. Choose the developers that are appropriate depending on your expectations.</p>
-                        <p>c. Discuss the project in detail and take updates from them about the work progress.</p>
-                    </div>,
+                <p>To hire developers from us, Clients have just to follow a few steps:</p>
+                <p>a. Approach us with the requirements of the project and developers in brief.</p>
+                <p>b. Choose the developers that are appropriate depending on your expectations.</p>
+                <p>c. Discuss the project in detail and take updates from them about the work progress.</p>
+            </div>,
             isShow: false
         },
         {
-            
+
             title: "How much does it cost to hire a Developer?",
             desc: "The cost can be estimated based on the project requirement and we have varied pricing based on per hour and per month and the client can choose as per their convenience.",
             isShow: false
@@ -45,15 +45,15 @@ const Faq = () => {
         <div className="mx-auto mb-10 max-w-7xl">
             <div className="flex justify-center p-2 md:p-0 h-[100px]">
                 <p className="text-[#3D4C61] text-[24px] sm:text-[35px] font-900 font-redHat flex justify-start items-center">
-                    <img src="/assets/squres.png" alt="squares" className="mb-2 mr-2 h-10" />
+                    <img src="/assets/squres.png" alt="squares" className="h-10 mb-2 mr-2" />
                     Faq
                 </p>
             </div>
             {faqData && faqData.map((faq, index) => (
-                <div className="p-8 mb-3 shadow-md" key={index}>
+                <div className="p-8 mb-3 shadow-md cursor-pointer" key={index} onClick={() => showFaq(index)}>
                     <div className="flex items-center justify-between">
                         <p className="text-[#747474] font-400 text-[19px] pr-3">{faq.title}</p>
-                        <img src="/assets/down-arrow.png" alt="arrow-down" className={`h-5 cursor-pointer ${faq.isShow ? "rotate-180" : "rotate-0"}`} onClick={() => showFaq(index)} />
+                        <img src="/assets/down-arrow.png" alt="arrow-down" className={`h-5 cursor-pointer ${faq.isShow ? "rotate-180" : "rotate-0"}`} />
                     </div>
                     <p className={`text-[#696969] font-400 text-[16px] mt-10 ${faq.isShow ? "block" : "hidden"}`}>
                         {faq.desc}
