d3e1b7b67ac0339ba32a2cb92e3de2f1f1b18da6
S Dinesh <d4dinesh52@gmail.com>
2022-12-19 11:03:53 +0530

    ad tag scripts added

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
     return (
@@ -23,8 +24,18 @@ export default function Document() {
                 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
                 <meta NAME="geo.placename" CONTENT="Tamil Nadu" />
                 <meta NAME="geo.region" CONTENT="India" />
+                <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+                    __html: `
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-PHH8JRF');
+                    ` }}>
+                </Script>
             </Head>
             <body>
+                <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PHH8JRF" height="0" width="0" style="display:none;visibility:hidden"></iframe>` }}></noscript>
                 <Main />
                 <NextScript />
             </body>
